28c777fd58e9c54a3f9b141b168509d5e2d3ca26
umaishasan <umaishasan96@gmail.com>
2021-09-02 17:59:30 +0500

    my first unity project practice

---
+++ Assets/Scripts/monoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monoBehaviour : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Debug.Log("Awake method call");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start method call");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update method call");
+    }
+
+    void onEnable()
+    {
+        Debug.Log("onEnable method call");
+    }
+
+    void onDisable()
+    {
+        Debug.Log("onDisable method call");
+    }
+}
