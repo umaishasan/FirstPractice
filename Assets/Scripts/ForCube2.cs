eec5c35f025e171589b506f736ba09c717ff969e
umaishasan <umaishasan96@gmail.com>
2021-09-03 19:34:43 +0500

    new update works

---
+++ Assets/Scripts/ForCube2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForCube2 : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Debug.Log("Awake method call");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start method call");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*transform.Translate(Vector3.forward*2* Time.deltaTime);*/
+        /*transform.Rotate(Vector3.forward * 2);*/
+        /*transform.Rotate(Vector3.up * 2);*/
+       /* transform.Rotate(Vector3.down * 2);*/
+        transform.Rotate(Vector3.back * 2);
+        Debug.Log("Update method call");
+    }
+}
--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -1,18 +1,124 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Test2 : MonoBehaviour
 {
+    private float speed = 10f;
+
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+    }
+
+    void Awake()
+    {
+        Debug.Log("Awake called");
+    }
+
+    void Reset()
+    {
+        Debug.Log("Reset called");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start called");
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("OnDisable called");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.objectMovingWithDT();
+        /*this.objectMovingWithSimple();*/
     }
+
+    //MOUSE EVENTS
+    private void OnMouseEnter()
+    {
+        Debug.Log("Mouse Enter");
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("Mouse Drag");
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log("Mouse Over");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse Down");
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Mouse Up");
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log("Mouse Up As Button");
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Mouse Exit");
+    }
+
+    void objectMovingWithDT()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-10f, 0f, 0f) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(10f, 0f, 0f) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0f, 0f, 10f) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0f, 0f, -10f) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(new Vector3(0f, 10f, 0f) * Time.deltaTime);
+        }
+    }
+
+    void objectMovingWithSimple()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * this.speed);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * this.speed);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * this.speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * this.speed);
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * this.speed);
+        }
+    }
+
 }
